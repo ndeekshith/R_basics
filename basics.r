d86bb7f44a6ab1e6ef3cdcbcfe07cdbf8da39341
Deekshith N <deekshithdeepith12042004@gmail.com>
2025-06-25 13:21:43 +0530

    updtaed some errors and added

--- basics.r
+++ basics.r
@@ -1,29 +1,123 @@
-library(datasets)
-#Reading datasets
-head(iris)
-# Plotting features
-plot(iris$Species)
-plot(iris$Petal.Length)
-plot(iris$Species,iris$Petal.Length)
-plot(iris$Petal.Length,iris$Petal.Width)
-plot(iris)
-#Plot with options
-plot(iris$Petal.Length, iris$Petal.Width,
-     col = "#cc0000",
-     pch = 19,
-     main = "Iris: Petal Lenght vs Width",
-     xlab = "Petal Length",
-     ylab = "Petal Width")
- # Plot formulaes with PLOT()
+# 1. Variables
 
-plot(cos,0,2*pi)
-plot(exp,1,10)
-plot(dnorm,-3,+3)
+#Assigning a number to variable
+number <- 10
+number
 
-plot(dnorm,-3,+3,
-     col = "#0000cc",
-     lwd = 5,
-     main = "Standered Normal distributions",
-     xlab = "z-scores",
-     ylab = "DEnsity")
+#Assigning a string to 
+variable <- "Hello"
+variable
 
+# We can do operations
+result <- 5 * 4
+result
+
+# 2. Data types
+x <-10.5
+class(x)
+y <- 5L
+class(y)
+name <- "Alice"
+class(name)
+
+# Data Structures
+
+# Numeric vectors
+numbers <- c(1,2,3,4,5,6)
+numbers
+
+# Character vectors
+fruits <- c("apple","mango","cheery")
+
+# MIxing all datatypes
+mix_vector <- c(1,"hello",TRUE)
+mix_vector
+# Aceesing the vectors same as python
+
+numbers[1]
+
+# Lists in R
+my_list <- list("john",40,TRUE,c(1,2,3))
+my_list
+
+# Creating the 2 X 3 matrix
+matrix1 = matrix(c(1,2,3,4,5,6),nrow = 2, ncol = 3, byrow = TRUE)
+matrix1
+
+# Creating a multi dimensional matrix
+my_array <- array(1:12,dim = c(2,3,2))
+my_array
+my_array[1,2,1]
+
+# Creating a data frame in R
+students_data <- data.frame(
+    Name = c("Alice","Bob","Charlie"),
+    Age = c(25,30,22),
+    Is_student = c(TRUE,FALSE,TRUE)
+)
+students_data
+
+# Acessing data frame elements
+students_data$Name
+students_data[,"Age"]
+
+# Print function
+my_variable <- "This is a message"
+print(my_variable)
+
+
+# Operators
+10 %% 3 # remainder
+5 > 3 # relational
+(TRUE & FALSE) # logical
+1:5 # sequence
+
+"apple" %in% c("banana","apple","cherry")
+
+# Conditional Statements
+age <- 18
+
+if (age >= 18) {
+    print("You are an adult")
+} else {
+    print("You are a minor") 
+}
+
+# Loops 
+fruits <- c("apple","mango","cherry")
+for (fruit in fruits) {
+    print(fruit)
+}
+
+# Loop through numbers
+for (i in 1:5) {
+    print(i * 2)
+}
+
+# While loop 
+count <- 1
+while (count <= 5) {
+    print(paste("Counts:",count))
+    count <- count + 1
+}
+
+# Functions 
+nums <- c(10,20,30,40)
+sum(nums)
+mean(nums)
+length(nums)
+sqrt(25)
+
+# Function
+say_hello <- function() {
+    print("Hello World")
+}
+say_hello()
+
+# Functions with arguments
+add_numbers <- function(a,b) {
+    results <- a + b
+    return(result)
+}
+sum_result <- add_numbers(5,7)
+print(sum_result)
